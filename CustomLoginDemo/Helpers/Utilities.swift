f79a956b2bc2627ec2d0d1b23251aa5193e730b2
xxxzzz112 <stylliam4@gmail.com>
2020-03-02 11:14:23 -0500

    color, regular expression, spell check

--- CustomLoginDemo/Helpers/Utilities.swift
+++ CustomLoginDemo/Helpers/Utilities.swift
@@ -19,7 +19,7 @@ class Utilities {
         
         bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
         
-        bottomLine.backgroundColor = UIColor.init(red: 255/255, green: 0/255, blue: 0/255, alpha: 1).cgColor
+        bottomLine.backgroundColor = UIColor.init(red: 187/255, green: 0/255, blue: 0/255, alpha: 1).cgColor
         
         // Remove border on text field
         textfield.borderStyle = .none
@@ -32,7 +32,7 @@ class Utilities {
     static func styleFilledButton(_ button:UIButton) {
         
         // Filled rounded corner style
-        button.backgroundColor = UIColor.init(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+        button.backgroundColor = UIColor.init(red: 187/255, green: 0/255, blue: 0/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
@@ -48,7 +48,7 @@ class Utilities {
     
     static func isPasswordValid(_ password : String) -> Bool {
         
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[.,$@$#!%*?&])[A-Za-z\\d$@$#!%*?&.,]{8,}")
         return passwordTest.evaluate(with: password)
     }
     
